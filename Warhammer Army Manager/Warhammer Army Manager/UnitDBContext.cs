8967ce7f804484970f8fcf70da84556513e28750
Maximilian Mewes <mewes.maximilian@gmx.de>
2023-09-19 20:35:12 +0200

    Test

---
+++ Warhammer Army Manager/Warhammer Army Manager/UnitDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warhammer_Army_Manager.Models;
+
+namespace Warhammer_Army_Manager
+{
+    internal class UnitDBContext : DbContext
+    {
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=products.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
--- Warhammer Army Manager/Warhammer Army Manager/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Warhammer Army Manager/Warhammer Army Manager/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Warhammer_Army_Manager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Warhammer Army Manager;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Warhammer Army Manager;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
