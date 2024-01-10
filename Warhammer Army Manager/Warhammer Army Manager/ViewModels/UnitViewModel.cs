3e9ed4304948f3904e1ef97d444d499e6391bad0
Marian Buhle <marianbuhle@gmail.com>
2024-01-10 19:38:32 +0100

    Tables for data

--- Warhammer Army Manager/Warhammer Army Manager/ViewModels/UnitViewModel.cs
+++ Warhammer Army Manager/Warhammer Army Manager/ViewModels/UnitViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Warhammer_Army_Manager.Database.Models;
 
 namespace Warhammer_Army_Manager.ViewModels
 {
     class UnitViewModel : ViewModel
     {
+        public ObservableCollection<Unit> Units { get; set; }
     }
 }
--- Warhammer Army Manager/Warhammer Army Manager/ViewModels/WeaponViewModel.cs
+++ Warhammer Army Manager/Warhammer Army Manager/ViewModels/WeaponViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Warhammer_Army_Manager.Database;
+using Warhammer_Army_Manager.Database.Models;
+using Warhammer_Army_Manager.Views;
 
 namespace Warhammer_Army_Manager.ViewModels
 {
     class WeaponViewModel : ViewModel
     {
+        public ObservableCollection<Weapon> Weapons { get; set; }
     }
 }
